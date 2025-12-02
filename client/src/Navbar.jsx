ec5f023fb08b468b35c6906921fc786776ad5f42
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2025-12-02 12:09:24 +0100

    Creato Navbar
    appena iniziato, non finito

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Navbar.css'
+
+function Navbar() {
+
+  return (
+    <>
+      <nav>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMatteo_Salvini&psig=AOvVaw1awxQLxSidVXF151DQunsU&ust=1764759259340000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPDk1NfenpEDFQAAAAAdAAAAABAE" className="logo" alt="Vite logo" />
+        <h1>Salvini</h1>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
