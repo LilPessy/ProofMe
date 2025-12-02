d56dc4e86bc921c3c1cc986631e68eb2b44c0e55
Angela <a.martoccia@studenti.poliba.it>
2025-12-02 12:22:18 +0100

    modificato Navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <>
       <nav>
         <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMatteo_Salvini&psig=AOvVaw1awxQLxSidVXF151DQunsU&ust=1764759259340000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPDk1NfenpEDFQAAAAAdAAAAABAE" className="logo" alt="Vite logo" />
-        <h1>Salvini</h1>
+        <h1>ProofMe</h1>
       </nav>
     </>
   )
