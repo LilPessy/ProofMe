b305bfdca12103d6d9f96f591c3ba983ede604cc
Angela <a.martoccia@studenti.poliba.it>
2025-12-05 11:02:16 +0100

    aggiunta foto profilo

---
+++ client/src/UserLogo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './UserLogo.css'; 
+import profilePic from './assets/danielephoto.png'
+
+function UserLogo() {
+  return (
+    <div className="profile-container">
+      <div className="image-wrapper">
+        <img src={profilePic} alt="Daniele Profile" className="profile-img" />
+      </div>
+      <h2 className="greeting">Ciao, Daniele</h2>
+    </div>
+  );
+}
+
+export default ProfileHeader;
