fa2e674a116623f82e4de545b0ea68537f2088cc
Angela <a.martoccia@studenti.poliba.it>
2025-12-05 12:27:53 +0100

    modifica in userlogo

--- client/src/UserLogo.jsx
+++ client/src/UserLogo.jsx
@@ -1,11 +1,11 @@
 import './UserLogo.css'; 
-import profilePic from './assets/danielephoto.png'
+import danielephoto from './assets/danielephoto.png'
 
 function UserLogo() {
   return (
     <div className="profile-container">
       <div className="image-wrapper">
-        <img src={danielephoto.png} alt="Daniele Profile" className="profile-img" />
+        <img src={danielephoto} alt="Daniele Profile" className="profile-img" />
       </div>
       <h2 className="greeting">Ciao, Daniele</h2>
     </div>
