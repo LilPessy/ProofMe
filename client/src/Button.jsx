07184ebd9ca8e95fbbe4679c40fc78d731255802
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2025-12-07 18:55:44 +0100

    Creato Button
    finito

---
+++ client/src/Button.jsx
@@ -0,0 +1,16 @@
+import './Button.css'; 
+
+function Button({content, callback, icon}) {
+
+
+  return (
+    <div className="button-container">
+        <button className="btn" onClick={callback}>
+            {content}
+            {icon ? <img src={icon} alt="Icon" className="btn-icon" />: null}
+        </button>
+    </div>
+  );
+}
+
+export default Button;
