eeee3edb3fdde3eb3b1752e2e3be01bad5a1310a
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2025-12-16 11:54:40 +0100

    finito homelogin

--- client/src/pages/HomeLogin.jsx
+++ client/src/pages/HomeLogin.jsx
@@ -3,8 +3,11 @@ import proofmeLogo from '../../public/Logo.svg'
 import './HomeLogin.css'
 import Navbar from '../components/Navbar';
 import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 function HomeLogin() {
+
+const navigate = useNavigate();
 
   return (
     <div className='content'>
@@ -12,8 +15,8 @@ function HomeLogin() {
       <img src={proofmeLogo} />
       <h2 style={{ color: '#0B1E45', fontSize: '50px', marginBottom: '5px' }}>ProofMe</h2>
       <p style={{  color: '#0B1E45', fontSize: '28px', marginTop: '0' }}>Carriera senza dubbi.</p>
-      <Button content="Login"/>
-      <Button content="Registrati"/>
+      <Button content="Login" callback={()=>navigate('/login')}/>
+      <Button content="Registrati" callback={()=>navigate('/signin')}/>
     </div>
   )
 }
