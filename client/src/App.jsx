4c8fa2966daa531ca471d5d029096c52dc33b22b
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2025-12-27 14:21:16 +0100

    Aggiunta rotta per Certificato

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Registrazione from './pages/Registrazione';
 import HomeLogin from './pages/HomeLogin';
 import './App.css';
+import Certificato from './pages/Certificato';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/signin" element={<Registrazione/>} />
+        <Route path="/certificato" element={<Certificato/>} />
 
       </Routes>
     </BrowserRouter>
