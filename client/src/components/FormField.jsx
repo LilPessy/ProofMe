465932a5021da9ae3cc30f3b37d2a0d199d8fe48
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2025-12-28 17:37:04 +0100

    Aggiunto max nel formfield per i campi numerici
    utile per il voto di laurea

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,14 +1,13 @@
 import './style/FormField.css';
 
-const FormField = ({label, type, placeholder, name, value, onChange}) => {
+const FormField = ({label, type, placeholder, name, value, max, onChange}) => {
   
     return (
       
           <div className="form-group">
             <label>{label}</label>
             {type === 'file' ? ( <input type="file" name={name} onChange={onChange} placeholder={placeholder} />) 
-                  : (<input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} />
-      )}
+                  : (<input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} max={max} />)}
 
           </div>
   
