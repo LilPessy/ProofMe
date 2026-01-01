d68438244f1fb7cf26a5a4e282d3fa3ce0fce181
Daniele Petti <75277685+LilPessy@users.noreply.github.com>
2026-01-01 20:09:59 +0100

    Modificata Navbar
    aggiunto hamburger menu con animazione con tasto per fare logout

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,35 @@
 
 import proofmeLogo from '../../public/Logo.svg'
 import hamburgerMenu from '../assets/hamburgerMenu.svg'
+import { useState } from 'react';
 import './style/Navbar.css'
 
 function Navbar({type}) {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userType');
+    window.location.href = '/';
+  }
 
   return (
     <>
       <nav>
         {type==='home'?(<img src={proofmeLogo} className="logo" alt="ProofMe logo" />):null}
         <h1>ProofMe</h1>
-        {type==='home'?(<img src={hamburgerMenu} className="logo" alt="hamburger logo" />):null}
+        {type==='home'?(<img src={hamburgerMenu} className="logo" alt="hamburger logo" onClick={toggleMenu} />):null}
+        {isMenuOpen && (
+          <div className="dropdown-menu">
+            <p onClick={() => {alert('Funzionalità in arrivo')}}>Notifiche</p>
+            <p onClick={logout}>Logout</p>
+          </div>
+        )}
       </nav>
     </>
   )
